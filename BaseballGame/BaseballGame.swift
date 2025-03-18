ef09a9502bae52fcdd83ec110c4c9af29b0f302d
ghnn-n <cgh6470@gmail.com>
2025-03-18 13:39:18 +0900

    Comment: 주석 추가

--- BaseballGame/BaseballGame.swift
+++ BaseballGame/BaseballGame.swift
@@ -7,16 +7,17 @@
 import Foundation
 
 struct BaseballGame {
-    var isSolved = true        // 유저가 정답을 맞췄는지 여부
-    var score: [String] = []
-    
+    var isSolved = true         // 유저가 정답을 맞췄는지 여부
     var numberOfGames = 0       // 몇 번째 게임인지 기록할 변수
     var numberOfTries = 0       // 몇 번째 시도 중인지 기록할 변수
     
-    mutating func setCorrect() -> [Int] {   // 정답을 생성하는 메서드
+    var score: [String] = []    // 게임 기록이 정리되서 할당될 변수
+    
+    // 정답을 생성하는 메서드
+    mutating func setCorrect() -> [Int] {
         var correct: [Int] = []
         
-        correct.append(Int.random(in: 1...9))   // 첫 번째 숫자는 0이 될 수 없음
+        correct.append(Int.random(in: 1...9))   // 첫 번째 숫자는 0이 될 수 없으므로 1~9
         
         while correct.count < 3 {               // correct에 숫자가 3개 담길 때까지
             let num = Int.random(in: 0...9)     // 0~9까지 중 랜덤한 숫자를 생성
@@ -27,7 +28,8 @@ struct BaseballGame {
         return correct
     }
     
-    mutating func checkInput(_ num: [Int]) -> Bool {    // 중복 값을 입력했는지 확인하는 메서드
+    // 중복 값을 입력했는지 확인하는 메서드
+    mutating func checkInput(_ num: [Int]) -> Bool {
         let numSet = Set(num)                           // 받아온 배열을 Set으로 변환
         if num.count == numSet.count {                  // Set으로 변환한 배열과 원래 배열의 count를 대조
             return true                                 // count가 같으면 true를 반환
@@ -36,6 +38,7 @@ struct BaseballGame {
         }
     }
     
+    // 게임 시작 전(로비?)
     mutating func play() {
         while isSolved {
             print("원하는 번호를 입력해주세요. \n"
@@ -45,44 +48,44 @@ struct BaseballGame {
             
             switch getNumber {
             case "1":
-                isSolved = false
+                isSolved = false        // 게임이 시작될 수 있도록 정답 처리를 false로 변경
                 numberOfGames += 1      // 시작을 누르면 게임 횟수 증가
                 gamePlay()
-            case "2":
+            case "2":                   // 스코어 출력
                 for i in score {
                     print(i)
                 }
-            case "3":
+            case "3":                   // 게임 설명
                 print("정답은 0~9까지의 수 중 겹치지 않는 세 자리의 숫자입니다. \n"
                       + "정답은 0으로 시작할 수 없습니다. \n"
                       + "입력하신 숫자 중 맞는 숫자는 strike, \n"
                       + "정답에 포함되긴 하지만 위치가 안 맞는 경우는 ball로 표시합니다. ")
             case "4":
-                isSolved = false
-            default:
+                isSolved = false        // 해당 루프문을 탈출
+            default:    // 잘못된 값을 입력 시
                 print("다시 입력하세요. ")
                 continue
             }
         }
     }
     
+    // 게임 시작
     mutating func gamePlay() {
         let correct = setCorrect()  // 정답 생성
         
-        // 게임 시작
         print("게임을 시작합니다. 세 자리 숫자를 입력하세요. \n"
               + "게임을 끝내고 싶으시면 [exit]을 입력하세요")
         
         while !isSolved {           // 유저가 답을 맞추지 못하면(false이면) 반복
-            print(correct)              // 테스트를 위해 정답을 알고 시작함(실제 게임에선 주석처리)
-            
             var strike = 0
             var ball = 0
+            
+            print(correct)              // 테스트를 위해 정답을 알고 시작함(실제 게임에선 주석처리)
             
             let input = readLine() ?? ""    // 유저의 입력값
             let inputArray: [Int] = input.map { Int(String($0)) ?? 0 }  // 유저의 입력값을 Int 배열로 반환
             
-            guard input != "exit" else { return }   // 유저가 직접 게임을 종료할 수 있게 함
+            guard input != "exit" else { return }   // 유저가 게임 도중 게임을 종료할 수 있게 함
             
             guard input.count == 3 &&           // 예외처리: 입력 값이 숫자인지, 3자리인지 체크
                     Int(input) != nil else {
@@ -120,8 +123,9 @@ struct BaseballGame {
             numberOfTries += 1      // 시도 횟수 1 증가
         }
         
-        score.append("\(numberOfGames)번째 게임: \(numberOfTries)번 시도")
-        numberOfTries = 0
-        play()             // 안내 문구 재 출력
+        // 정답을 맞춰서 루프문을 빠져나옴
+        score.append("\(numberOfGames)번째 게임: \(numberOfTries)번 시도") // 스코어를 기록
+        numberOfTries = 0   // 시도 횟수를 초기화
+        play()              // 로비 재입장
     }
 }
